ebaf664a9dc2bba6b5889df6e9bd85226bcf504f
aniii-coder <aksingh22356@gmail.com>
2024-08-27 18:54:27 +0530

    Withdraw feature added successfully

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
-pin = int(input("Enter the Pin: "))
+print("Welcome to the ATM !!")
+pin = int(input("Enter the New Pin to Register: "))
 balance = 987654
+scrt_key = int(input("Enter the Key: "))
 class ATM:
     def __init__(self,pin,balance):
         self.pin = pin 
         self.balance = balance
     def wtdrw_blnc(self):
-        amt = int(input("Enter the amount yo want to withdraw: "))
-        self.balance = self.balance - amt
-        return self.balance
-    def deposit_blnc(self):
-        amt = int(input("Enter the amount yo want to Deposit: "))
-        self.balance=self.balance + amt
-        return self.balance
-    def pin_chng(self):
-        nw_pin = int(input("Enter the new pin : "))
-        self.pin = nw_pin
-        return self.pin
+        amt1 = int(input("Enter the amount yo want to withdraw: "))
+        self.balance = self.balance - amt1
+        print("Transaction succesfull of ${:,.2f}".format(amt1))
+        ttl_blnce = "Total Balance is ${:,.2f}".format(self.balance)
+        return ttl_blnce
+    # def deposit_blnc(self):
+    #     amt = int(input("Enter the amount yo want to Deposit: "))
+    #     self.balance=self.balance + amt
+    #     return self.balance
+    # def pin_chng(self):
+    #     nw_pin = int(input("Enter the new pin : "))
+    #     self.pin = nw_pin
+    #     return self.pin
 
-opt1= ATM(pin, balance)
-# new_pin = opt1.pin_chng()
-# print("New pin is: ",new_pin)
-# withdraw = opt1.wtdrw_blnc()
-current_balance = opt1.wtdrw_blnc()
-print("Total balnce is ${:,.2f}".format(current_balance))
+
+opt1 = ATM(pin,balance)
+
+
+while(True):
+    if scrt_key == pin:
+        opt = int(input(f"1.Withdraw\n2.Deposit\n3.Account Balance\n4.Transaction history\n5.Change pin\n6.Exit\nChoose the option from the following: "))
+        print(opt)
+        if opt == 1:
+            blnc = opt1.wtdrw_blnc()
+            print(blnc)
+        if opt == 6:
+            break
+    else:
+        print("Wrong Pin Entered !")
+        break
