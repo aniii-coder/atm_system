d67a01efb61d806de03e01ab692a5497a25c27ed
aniii-coder <aksingh22356@gmail.com>
2024-08-27 08:54:42 +0530

    Withdraw balance function is created

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+pin = 1234
+balance = 987654
+def wtdrw_blnc():
+    global pin,balance
+    amt = int(input("Enter the amount yo want to withdraw: "))
+    balance = balance - amt
+    return balance
+
+ct_blnc = wtdrw_blnc()
+print("Total balnce is ${:,.2f}".format(ct_blnc))
